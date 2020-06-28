ed3f1b603ecec012c2b9ae55aafe5aa4ed166fd0
Derek Zeng <derek.zeng@ninjavan.co>
2020-06-28 08:12:33 +0800

    Subscribable

--- eventbus.ts
+++ eventbus.ts
@@ -22,14 +22,19 @@ class EventBusController {
 
   constructor() {
     this.outPorts = []
-    this.getAllChildContexts().forEach(this.initChildContext.bind(this))
+    this.addChild(window)
   }
 
-  broadcast(msg) {  
+  addChild(context) {
+    this.initChildContext(context)
+  }
+
+  private broadcast(msg) {  
     for (const out of this.outPorts) {
       out.send(msg)
     }
   } 
+
   private initChildContext(context) {
     const channel = new MessageChannel()
     if (context === window) {
@@ -51,36 +56,53 @@ class EventBusController {
       this.broadcast(data)
     }
   }
-
-  private getAllChildContexts() {
-    return [...document.querySelectorAll('iframe'), window]
-  }
 }
 
+type ICallback = (any) => void
+type IResponder = (...any) => Promise<any>
 
-export class EventBus {
-  private get isParent () {
-    return window.parent === window
-  }
+interface IEventBus  {
+  handshake: (MessageEvent) => void
+  send: (any) => void
+  request: (key: string, args?: any[]) => Promise<any>
+  register: (key: string, IResponder) => void
+  subscribe: (evName: string, ICallback) => void
+  unsubscribe: (evName: string, ICallback) => void
+}
+
+interface IOption {
+  callback?: ICallback
+  name?: string
+}
+
+export class EventBus implements IEventBus {
 
   private id: number
-  private callback: (any) => void
+  private callback?: (any) => void
   private outPort: OutPort
-  private controller: EventBusController
+  private controller?: EventBusController
+  private name: string
+  private subscriptions: Map<string, ICallback[]>
 
-  constructor({callback}) {
+  constructor({callback, name}: IOption = {} ) {
     if (this.isParent) {
       this.controller = new EventBusController()
     }
     this.callback = callback
+    this.name = name
+    this.subscriptions = new Map()
     window.addEventListener('message', this.handshake)
+  }
+
+  private get isParent () {
+    return window.parent === window
   }
 
   handshake = (e) => {
     const { type, id } = e.data
     if (type === 'init') {
       this.id = id
-      this.outPort = new OutPort(e.ports[0], this.callback)
+      this.outPort = new OutPort(e.ports[0], this.onMessage.bind(this))
       this.outPort.send({
         type: 'ready',
         id,
@@ -89,7 +111,39 @@ export class EventBus {
     }
   }
 
+  private onMessage(e) {
+    const { type } = e.data
+    if (this.subscriptions.has(type)) {
+      this.subscriptions.get(type).forEach(cb => cb(e))
+    }
+    this.callback && this.callback(e)
+  }
+
   send(msg) {
     this.outPort.send(msg)
+  }
+
+  request(key) {
+    return Promise.resolve(true)
+  }
+
+  register(key, responder) {
+  }
+
+  subscribe(evName, cb) {
+    if (!this.subscriptions.has(evName)) { 
+      this.subscriptions.set(evName, [])
+    }
+    this.subscriptions.get(evName).push(cb)
+  }
+
+  unsubscribe(evName, cb) {
+    if (!this.subscriptions.has(evName)) { 
+      return false
+    }
+    const idx = this.subscriptions.get(evName).indexOf(cb)
+    if (idx >= 0) {
+      this.subscriptions.get(evName).splice(idx, 1)
+    }
   }
 }
